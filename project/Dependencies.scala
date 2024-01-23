dc85c78037da561290b34149583d6126ce73fc26
nmcb <nmcb@xs4all.nl>
2024-01-23 14:05:02 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val akkaVersion                     = "2.6.9"
   val akkaAlpakkaVersion              = "2.0.1"
   val logbackVersion                  = "1.2.3"
-  val catsCoreVersion                 = "2.1.1"
+  val catsCoreVersion                 = "2.10.0"
   val kindProjectorPluginVersion      = "0.11.0"
   val akkaPersistenceCassandraVersion = "1.0.1"
   val datastaxCassandraDriverVersion  = "4.9.0"
